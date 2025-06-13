fc34d8745bb6022a3e3f63e9187a5a14c248a808
SaiHarika16 <saiharika2005@gmail.com>
2025-06-13 22:26:30 +0530

    Implement and test backend product routes: create, view, update, delete

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,21 @@
 //server.js
 import express from "express";
 import dotenv from "dotenv";
-import Product from "./models/product.js";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app=express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.post("/api/products",async(req,res)=>{
-    const product=req.body;
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({success:false, message: "All fields are required" });
-    }
-    const newProduct=new Product(product)
-
-    try{
-        await newProduct.save();
-        res.status(201).json({success:true, data: newProduct});
-    }catch(error){
-        console.error("Error creating product:", error);
-        res.status(500).json({success:false, message:"Server error while creating product"});
-    }
-})
-
 //console.log(process.env.MONGO_URI);
+
+app.use("/api/products", productRoutes);
 
 app.listen(5000,()=>{
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:"+ PORT);
 })
